cb40bfaae250b544de6ea14010dc75294609a0df
Charly Park <cpark@riseup.net>
2025-06-21 13:18:31 -0400

    File I/O changes
    Commented append out of the program file and added to journal. Added pathing and import OS for cross platform functionality. Fleshing out the actual journal functions.

--- Journal/journal.py
+++ Journal/journal.py
@@ -1,9 +1,20 @@
+import os
+
+
 def load(name):
     # todo: populate from file if it exists
     return []
 
 def save(name, journal_data):
-    pass
+    filename = os.path.abspath(os.path.join('./journals/' , name + '.jrl'))
+    print("......saving to: {}".format(filename))
+    
+    fout = open(filename, 'w') # file output string
+
+    for entry in journal_data:
+        fout.write(entry)
+
+    fout.close()    
 
 def add_entry(text, journal_data):
-    return None
+    journal_data.append(text)
