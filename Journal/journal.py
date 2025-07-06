180ad8d076a284ff0ecddd6e7f718e2093db4d57
Charly Park <cpark@riseup.net>
2025-07-06 17:40:15 -0400

    Update journal.py
    Cleaned up the file output string. Added a method for the filepath.

--- Journal/journal.py
+++ Journal/journal.py
@@ -1,20 +1,29 @@
 import os
 
-
 def load(name):
     # todo: populate from file if it exists
-    return []
+    data = []
+    filname = get_full_pathname(name)
+
+    if os.path.exists(filename):
+        with open(filename) as fin:
+            for entry in fin.readlines():
+                data.append(entry.rstrip())
+
+    return data
 
 def save(name, journal_data):
-    filename = os.path.abspath(os.path.join('./journals/' , name + '.jrl'))
+    filename = get_full_pathname(name)
     print("......saving to: {}".format(filename))
-    
-    fout = open(filename, 'w') # file output string
 
-    for entry in journal_data:
-        fout.write(entry)
+    with open(filename, 'w') as fout:
+        for entry in journal_data:
+            fout.write(entry + '\n')
 
-    fout.close()    
+def get_full_pathname(name):
+    filename = os.path.abspath(os.path.join('.', 'journals', name + '.jrl'))
+    return filename
+
 
 def add_entry(text, journal_data):
     journal_data.append(text)
